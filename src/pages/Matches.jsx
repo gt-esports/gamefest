0d43412f4091367a10b9041b210b9be118b74ac8
Ethan Loo Yu Teng <eloo6@gatech.edu>
2025-02-14 17:46:17 -0500

    finished most of the skeleton

--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -5,64 +5,92 @@ function Matches() {
       <>
         <div className="w-dvw h-dvh">
           <Navbar/>
-          <main>
+          <main className="pb-10">
               <h1 
                   style={{fontFamily: 'Bayon, sans-serif'}} 
-                  className="py-10 text-9xl font-normal text-tech-gold xs:rounded-lg xs:text-5xl xs:backdrop-blur-lg sm:text-7xl md:text-8xl lg:text-9xl text-center">
+                  className="py-10 text-7xl font-normal text-tech-gold xs:rounded-lg xs:text-4xl xs:backdrop-blur-lg sm:text-5xl md:text-6xl lg:text-7xl text-center">
                   TEAM A VS. TEAM B
               </h1>
-                <div className="relative bg-linear-to-b from-[#08004E] to-[#101010] px-20 pb-20 w-3/4 items-center justify-center mx-auto rounded-lg">
-                  <div className="flex flex-row items-center justify-between h-96">
-                    <div className="flex flex-col h-full">
+              
+              <div className="relative flex flex-col bg-linear-to-b from-[#08004E]/[0.76] to-[#101010]/[0.28] px-4 w-8/9 items-center justify-center mx-auto rounded-lg">
+                  {/* Match */}
+                  <div className="flex flex-row items-stretch justify-between h-auto min-w-7/8 py-10">
+                    <div className="flex flex-col h-auto min-w-2/7">
                       <h1 
                           style={{fontFamily: 'Bayon, sans-serif'}} 
-                          className="p-10 text-9xl font-normal text-tech-gold xs:rounded-lg xs:text-5xl xs:backdrop-blur-lg sm:text-7xl md:text-8xl lg:text-9xl text-center">
+                          className="p-4 text-2xl text-center">
                           TEAM A
                       </h1>
-                      <div className="bg-[#90CDFF12] rounded-lg h-full p-10">
+                      <div className="bg-[#90CDFF]/[0.12] rounded-lg h-full p-auto drop-shadow-2xl">
                         <h1 
                             style={{fontFamily: 'Bayon, sans-serif'}} 
-                            className="py-10 text-9xl font-normal text-tech-gold xs:rounded-lg xs:text-3xl xs:backdrop-blur-lg sm:text-5xl md:text-5xl lg:text-7xl text-center">
+                            className="p-8 text-2xl text-center align-middle">
                             TEAM ROSTER
                         </h1>
                       </div>
                     </div>
-                    <div className="bg-linear-to-b from-[#607D9576] to-[#66717A28] p-10 rounded-lg h-auto w-1/3 px-10 ">
+                    <div className="flex flex-col justify-center bg-linear-to-b from-[#607D95]/[0.76] to-[#66717A]/[0.28] p-10 rounded-lg h-auto min-w-2/7 px-10 items-center drop-shadow-2xl">
+                      <img 
+                        src="" 
+                        alt="Game Logo"
+                        className="bg-gray size-fit rounded-lg" />
                       <h1 
                           style={{fontFamily: 'Bayon, sans-serif'}} 
-                          className="p-10 text-9xl font-normal text-tech-gold xs:rounded-lg xs:text-5xl xs:backdrop-blur-lg sm:text-7xl md:text-8xl lg:text-9xl text-center">
+                          className="p-4 text-2xl text-center">
                           GAME: XYZ
                       </h1>
                       <h1 
                           style={{fontFamily: 'Bayon, sans-serif'}} 
-                          className="p-10 text-9xl font-normal text-tech-gold xs:rounded-lg xs:text-5xl xs:backdrop-blur-lg sm:text-7xl md:text-8xl lg:text-9xl text-center">
+                          className="p-4 text-2xl text-center"> 
                           ROUND: XX
                       </h1>
                       <h1 
                           style={{fontFamily: 'Bayon, sans-serif'}} 
-                          className="p-10 text-9xl font-normal text-tech-gold xs:rounded-lg xs:text-md xs:backdrop-blur-lg sm:text-lg md:text-xl lg:text-2xl text-center">
+                          className="p-4 text-2xl text-center">
                           DATE: XX / XX / XXXX
                       </h1>
                     </div>
-                    <div className="flex flex-col h-full">
+                    <div className="flex flex-col h-auto min-w-2/7">
                       <h1 
                           style={{fontFamily: 'Bayon, sans-serif'}} 
-                          className="p-10 text-9xl font-normal text-tech-gold xs:rounded-lg xs:text-5xl xs:backdrop-blur-lg sm:text-7xl md:text-8xl lg:text-9xl text-center">
+                          className="p-4 text-2xl text-center">
                           TEAM B
                       </h1>
-                      <div className="bg-[#90CDFF12] rounded-lg h-full p-10">
+                      <div className="bg-[#90CDFF]/[0.12] rounded-lg h-full p-auto align-middle drop-shadow-2xl">
                         <h1 
                             style={{fontFamily: 'Bayon, sans-serif'}} 
-                            className="py-10 text-9xl font-normal text-tech-gold xs:rounded-lg xs:text-3xl xs:backdrop-blur-lg sm:text-5xl md:text-5xl lg:text-7xl text-center">
+                            className="p-8 text-2xl text-center">
                             TEAM ROSTER
                         </h1>
                       </div>
                     </div>
                   </div>
 
-                </div>
+                  {/* Live score */}
+                  <div className="flex flex-col items-stretch justify-stretch h-auto min-w-full py-10">
+                  <h1 
+                      style={{fontFamily: 'Bayon, sans-serif'}} 
+                      className="py-10 text-5xl font-normal xs:rounded-lg xs:text-xl xs:backdrop-blur-lg sm:text-2xl md:text-3xl lg:text-5xl text-center">
+                      LIVE SCORE
+                  </h1>
+                  <div className="flex bg-linear-to-b from-[#F1CCFF]/[0.17] to-[#101010]/[0.28] px-30 w-full h-20 items-center justify-center mx-auto rounded-lg">
+                  </div>
+                  
+                  </div>
+
+                  {/* Livestream */}
+                  <div className="flex justify-center h-auto min-w-full py-10">
+                      <h1 
+                          style={{fontFamily: 'Bayon, sans-serif'}} 
+                          className="py-10 text-5xl font-norma xs:rounded-lg xs:text-2xl bg-black  w-[1024px] h-[480px] sm:text-3xl md:text-4xl lg:text-5xl text-center align-middle">
+                          LIVESTREAM
+                      </h1>
+                  </div>
+              </div>
+
+              
           </main>
-          <Footer />
+          {/* <Footer /> */}
         </div>
       </>
     );
