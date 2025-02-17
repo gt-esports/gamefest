b72ec0a41b968755db73b7c72599238bcf8e0558
GaoWilson81 <wilsongao2002@gmail.com>
2025-02-17 00:00:40 -0500

    Clickable link
    Add some examples of clickable links for sponsors 1-5

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -2,22 +2,27 @@ export const sponsors = [
     {
         src: 'Sponsor 1',
         alt: 'Sponsor 1',
+        link: 'https://www.example.com'
     },
     {
         src: 'Sponsor 2',
         alt: 'Sponsor 2',
+        link: 'https://www.example.com'
     },
     {
         src: 'Sponsor 3',
         alt: 'Sponsor 3',
+        link: 'https://www.example.com'
     },
     {
         src: 'Sponsor 4',
         alt: 'Sponsor 4',
+        link: 'https://www.example.com'
     },
     {
         src: 'Sponsor 5',
         alt: 'Sponsor 5',
+        link: 'https://www.example.com'
     },
     {
         src: 'Sponsor 6',
