4e285226ee31858ea6e57a80db611985bc6d91ea
Jim Chen <loxijchen@gmail.com>
2025-02-17 19:15:47 -0500

    complete scroll functionality, changes scrollbar styles

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,45 @@ import Teams from "./pages/Teams.tsx";
 import Sponsor from "./pages/Sponsor.tsx";
 import About from "./pages/About.tsx";
 
+const scrollbarStyles = `
+  ::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: rgba(212, 179, 86, 0.5);  /* tech-gold with opacity */
+    border-radius: 3px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: rgba(212, 179, 86, 0.8);
+  }
+`;
+
 function App() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/brackets" element={<Brackets />} />
-        <Route path="/matches" element={<Matches />} />
-        <Route path="/teams" element={<Teams />} />
-        <Route path="/sponsor" element={<Sponsor />} />
-          <Route path="*" element={<Navigate to="/home" />} />
-        </Routes>
-      </main>
-    </div>
+    <>
+      <style>{scrollbarStyles}</style>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/brackets" element={<Brackets />} />
+            <Route path="/matches" element={<Matches />} />
+            <Route path="/teams" element={<Teams />} />
+            <Route path="/sponsor" element={<Sponsor />} />
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        </main>
+      </div>
+    </>
   );
 }
 
