2cea2255c612fa0b9dda0649cf22bb354ddf948d
Li Mei <mli783@gatech.edu>
2025-02-17 20:23:39 -0500

    livestream and footer

--- src/pages/Sponsor.tsx
+++ src/pages/Sponsor.tsx
@@ -1,8 +1,12 @@
+import Footer from "../components/Footer";
 function Sponsor() {
   return (
     <div className="h-screen w-full">
       <div className="flex h-full w-full bg-streak bg-cover pt-20 text-white">
         <a>This is the teams page.</a>
+      </div>
+      <div className="pt-80">
+          <Footer />
       </div>
     </div>
   );
