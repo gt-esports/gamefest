91db183442a2d700a2c571cf50601d69e578a63d
GaoWilson81 <wilsongao2002@gmail.com>
2025-02-23 14:56:41 -0500

    Team Page Mobile Optimized
    - Logo, Game, Team cards
    - Mobile view collapses the cards into one card with carousel format
    -Search bar only displays example schools in dropdown for now
    -Mobile view team button will be replaced with clickable link that leads to match page.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -9,16 +9,16 @@ const SearchBar = () => {
   );
 
   return (
-    <div className="relative w-96 pl-20">
+    <div className="relative w-full max-w-xs mx-20">
       <input
         type="text"
         placeholder="Search..."
-        className="w-full p-4 border border-gray-300 rounded-lg bg-transparent font-bayon text-2xl"
+        className="w-full p-3 border border-gray-300 rounded-lg bg-transparent font-bayon text-2xl"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
       {search && (
-        <ul className="relative right-0 mt-2 bg-gray-500 border border-gray-500 rounded-lg shadow-lg">
+        <ul className="relative mt-2 bg-gray-500 border border-gray-500 rounded-lg shadow-lg">
           {filterSchool.map((school, index) => (
             <li key={index} className="p-2 text-white font-bayon text-2xl hover:bg-gray-700">
               {school}
