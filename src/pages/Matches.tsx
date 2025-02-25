4663ea9742a2fc2078fc0d6c3db0f3ed9fa274e1
Jim Chen <loxijchen@gmail.com>
2025-02-25 15:50:14 -0500

    fix matches.css affecting global styles and fix footer for matches page

--- src/pages/Matches.tsx
+++ src/pages/Matches.tsx
@@ -10,86 +10,86 @@ import LiveScoreStatistics from "../components/LiveScoreStatistics"; // Adjust p
 
 function Matches() {
   return (
-    <>
-      <div className="flex flex-col min-h-screen w-full bg-streak bg-cover pt-20 text-white">
-        {/* Match Title */}
-        <h1 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>TEAM A VS. TEAM B</h1>
-        
-        <div className="backdrop">
-          {/* Match Details & Rosters */}
-          <div className="match-container flex justify-center items-center w-full py-10">
-            <div className="flex flex-row justify-between items-stretch w-11/12 max-w-7xl bg-black/30 p-8 rounded-lg">
-              
-              {/* Team A */}
-              <div className="team-card w-1/4 flex flex-col items-center flex-grow">
-                <h2 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>TEAM A</h2>
-                {fakeRoster.map((player, index) => (
-                  <TeamCard
-                    key={index}
-                    player1={player.player1}
-                    player2={player.player2}
-                    player3={player.player3}
-                    player4={player.player4}
-                    player5={player.player5}
-                  />
-                ))}
-              </div>
+    <div className="bg-streak bg-cover">
+        <div className="matchpage flex flex-col min-h-screen w-full pt-20 text-white">
+          {/* Match Title */}
+          <h1 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>TEAM A VS. TEAM B</h1>
+          
+          <div className="backdrop">
+            {/* Match Details & Rosters */}
+            <div className="match-container flex justify-center items-center w-full py-10">
+              <div className="flex flex-row justify-between items-stretch w-11/12 max-w-7xl bg-black/30 p-8 rounded-lg">
+                
+                {/* Team A */}
+                <div className="team-card w-1/4 flex flex-col items-center flex-grow">
+                  <h2 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>TEAM A</h2>
+                  {fakeRoster.map((player, index) => (
+                    <TeamCard
+                      key={index}
+                      player1={player.player1}
+                      player2={player.player2}
+                      player3={player.player3}
+                      player4={player.player4}
+                      player5={player.player5}
+                    />
+                  ))}
+                </div>
 
-              {/* Match Card */}
-              <div className="match-card w-1/3 flex flex-col items-center p-5 flex-grow">
-                {fakeMatches.map((match, index) => (
-                  <MatchCard
-                    key={index}
-                    game={match.game}
-                    round={match.round}
-                    date={match.date}
-                  />
-                ))}
-              </div>
+                {/* Match Card */}
+                <div className="match-card w-1/3 flex flex-col items-center p-5 flex-grow">
+                  {fakeMatches.map((match, index) => (
+                    <MatchCard
+                      key={index}
+                      game={match.game}
+                      round={match.round}
+                      date={match.date}
+                    />
+                  ))}
+                </div>
 
-              {/* Team B */}
-              <div className="team-card w-1/4 flex flex-col items-center flex-grow">
-                <h2 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>TEAM B</h2>
-                {fakeRoster.map((player, index) => (
-                  <TeamCard
-                    key={index}
-                    player1={player.player1}
-                    player2={player.player2}
-                    player3={player.player3}
-                    player4={player.player4}
-                    player5={player.player5}
-                  />
-                ))}
-              </div>
+                {/* Team B */}
+                <div className="team-card w-1/4 flex flex-col items-center flex-grow">
+                  <h2 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>TEAM B</h2>
+                  {fakeRoster.map((player, index) => (
+                    <TeamCard
+                      key={index}
+                      player1={player.player1}
+                      player2={player.player2}
+                      player3={player.player3}
+                      player4={player.player4}
+                      player5={player.player5}
+                    />
+                  ))}
+                </div>
 
+              </div>
             </div>
-          </div>
-        {/* Live Score Statistics */}
-          <div className="w-full flex flex-col items-center justify-center pt-16">
-            <h2 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>
-              LIVE SCORE
-            </h2>
-            <div className="flex flex-col from-[#F1CCFF]/[0.17] to-[#101010]/[0.28] px-10 w-3/4 items-center justify-center mx-auto rounded-lg mt-8 py-10 mb-20">
-              <LiveScoreStatistics stats={fakeStats} />
+          {/* Live Score Statistics */}
+            <div className="w-full flex flex-col items-center justify-center pt-16">
+              <h2 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>
+                LIVE SCORE
+              </h2>
+              <div className="flex flex-col from-[#F1CCFF]/[0.17] to-[#101010]/[0.28] px-10 w-3/4 items-center justify-center mx-auto rounded-lg mt-8 py-10 mb-20">
+                <LiveScoreStatistics stats={fakeStats} />
+              </div>
             </div>
-          </div>
-          {/* Twitch Live Stream Embed */}
-          <h2 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>LIVESTREAM</h2>
-          <div className="twitch-container mb-40">
-            <iframe
-              src="https://player.twitch.tv/?channel=mooda&parent=localhost"
-              allowFullScreen
-              allow="autoplay *; encrypted-media *;"
-            ></iframe>
+            {/* Twitch Live Stream Embed */}
+            <h2 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>LIVESTREAM</h2>
+            <div className="twitch-container mb-40">
+              <iframe
+                src="https://player.twitch.tv/?channel=mooda&parent=localhost"
+                allowFullScreen
+                allow="autoplay *; encrypted-media *;"
+              ></iframe>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Footer */}
-      <div className="pt-0">
-        <Footer />
+        {/* Footer */}
+        <div className="pt-8">
+          <Footer />
+        </div>
       </div>
-    </>
   );
 }
   
