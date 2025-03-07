23b9940e379a36776521a1eb8c9c643b2b6aee3c
Ethan Loo Yu Teng <eloo6@gatech.edu>
2025-03-07 01:40:58 -0500

    Add link support to Avatar component and implement Sponsor page layout

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,18 +3,23 @@ import React from 'react';
 interface AvatarProps {
   src: string;
   alt: string;
+  link: string;
   className?: string;
 }
 
-const Avatar: React.FC<AvatarProps> = ({ src, alt = "Sponsor logo", className }) => {
+const Avatar: React.FC<AvatarProps> = ({ src, alt = "Sponsor logo", link="/", className }) => {
   return (
-    <div className={`flex items-center justify-center w-20 h-20 rounded-full overflow-hidden mx-4 mb-10 bg-[#d9d9d9] ${className || ''}`}>
-      {src ? (
-        <img src={src} alt={alt} className="w-full h-full object-cover" />
-      ) : (
-        <span className="text-center font-medium">{alt}</span>
-      )}
-    </div>
+    <a href={link}>
+      <div className={`flex items-center justify-center w-20 h-20 rounded-full overflow-hidden mx-4 mb-10 bg-[#d9d9d9] ${className || ''}`}>
+        
+        {src ? (
+          <img src={src} alt={alt} className="w-full h-full object-cover" />
+        ) : (
+          <span className="text-center font-medium">{alt}</span>
+        )}
+        
+      </div>
+    </a>
   );
 };
 
--- src/pages/Sponsor.tsx
+++ src/pages/Sponsor.tsx
@@ -1,11 +1,27 @@
 import Footer from "../components/Footer";
+import Avatar from "../components/Avatar";
+import { sponsors } from "../data/sponsors";
+import '../styles/Matches.css';
+import '../styles/Sponsor.css';
+
 function Sponsor() {
   return (
-    <div className="h-screen w-full">
-      <div className="flex h-full w-full bg-streak bg-cover pt-20 text-white">
-        <a>This is the teams page.</a>
+    <div className="bg-streak bg-cover">
+      <div className="sponsor flex flex-col min-h-screen w-full pt-20 text-white">
+        <h1 className="text-5xl text-center py-6" style={{ fontFamily: 'Bayon, sans-serif' }}>SPONSORS</h1>
+
+        <div className="teamcard w-4/5 flex flex-row items-center flex-grow mx-auto">
+          {sponsors.map((sponsor, index: number): JSX.Element => (
+            <Avatar
+              key={index}
+              src={sponsor.src}
+              alt={sponsor.alt}
+              link={sponsor.link}
+            />
+          ))}
+        </div>
       </div>
-      <div className="pt-80">
+      <div className="pt-8">
           <Footer />
       </div>
     </div>
