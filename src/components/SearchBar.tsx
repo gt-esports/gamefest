fd9f9aeeccb03f36cc304885969e7d23109dab71
Ethan Loo Yu Teng <eloo6@gatech.edu>
2025-03-27 17:36:26 -0400

    .

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,14 +6,14 @@ interface SearchProps {
 
 const SearchBar: React.FC<SearchProps> = ({ onSearch }) => {
   const [search, setSearch] = useState("");
-  const schools = ["School A", "School B", "School C", "School D", "School E", "School F", "School G"];
+  const games = ["Counter Strike", "League of Legends", "Valorant", "Dota 2", "Fortnite", "Overwatch", "Brawl Stars"];
 
-  const filterSchool = schools.filter((school) =>
-    school.toLowerCase().includes(search.toLowerCase())
+  const filterGame = games.filter((game) =>
+    game.toLowerCase().includes(search.toLowerCase())
   );
 
-  const handleSearch = (school: string) => {
-    onSearch(school.toLowerCase());
+  const handleSearch = (game: string) => {
+    onSearch(game.toLowerCase());
     setSearch("");
   };
 
@@ -27,15 +27,15 @@ const SearchBar: React.FC<SearchProps> = ({ onSearch }) => {
       onChange={(e) => setSearch(e.target.value)}
       onKeyDown={(e) => (e.key === "Enter" ? handleSearch(search) : null)}
       />
-      {search && filterSchool.length > 0 && (
+      {search && filterGame.length > 0 && (
       <ul className="absolute top-full left-0 mt-2 w-full z-50 bg-gradient-to-br from-[#233a6d]/80 to-[#472b2b]/80 border border-gray-500 rounded-lg shadow-lg">
-        {filterSchool.map((school, index) => (
+        {filterGame.map((game, index) => (
         <li
           key={index}
           className="p-2 text-white text-left font-bayon text-2xl hover:bg-gray-700 cursor-pointer"
-          onClick={() => handleSearch(school)}
+          onClick={() => handleSearch(game)}
         >
-          {school}
+          {game}
         </li>
         ))}
       </ul>
