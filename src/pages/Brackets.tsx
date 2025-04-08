a17786236afa27901b80b0ba97ec0cf84908331b
Li Mei <mli783@gatech.edu>
2025-04-08 18:00:10 -0400

    brackets FOR NOW

--- src/pages/Brackets.tsx
+++ src/pages/Brackets.tsx
@@ -1,14 +1,22 @@
+import Brac from "../components/Brac";
 import Footer from "../components/Footer";
+import { useState } from "react";
+const games: string[] = Array.from({ length: 16 }, (_, i) => `Game ${i + 1}`);
 function Brackets() {
+  const user = { role: "Staff" }; //get from user data later
+  const [selectedGame, setSelectedGame] = useState("Game 1");
   return (
-    <div className="h-screen w-full">
-      <div className="flex h-full w-full bg-streak bg-cover pt-20 text-white">
-        <a>This is the brackets page.</a>
+    <div className="h-screen w-full relative flex flex-col">
+      <div className="flex flex-row flex-grow h-full"></div>
+      <div className="mt-4 text-lg font-semibold text-center">Selected: {selectedGame}</div>
+      <div className="flex h-full w-full justify-center items-center bg-streak bg-cover pt-20 text-white">
+        <Brac game={selectedGame} user={user} />
       </div>
-      <div className="pt-80">
-          <Footer />
+      <div className="pt-8">
+        <Footer />
       </div>
     </div>
   );
 }
+
 export default Brackets;
