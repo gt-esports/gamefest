4da9f4be786bfb514a62f4b8f4b4e31320d0bc6f
Ethan Loo Yu Teng <eloo6@gatech.edu>
2025-04-11 16:53:43 -0400

    fix: adjust indentation and structure in Information and Sponsor components

--- src/pages/Sponsor.tsx
+++ src/pages/Sponsor.tsx
@@ -6,10 +6,10 @@ function Sponsor() {
     <div className="flex w-full bg-streak flex-col bg-cover">
       <div className="w-full mx-auto text-white text-center pt-36 p-8 px-10 lg:px-20">
         <h1 className="justify-center font-bayon text-6xl pb-4">Sponsors</h1>
-        <div className='mx-auto bg-gray-800/80 p-8 items-center rounded-lg'>
+      </div>
+      <div className=' w-4/5 mx-auto bg-gray-800/80 p-8 items-center rounded-lg pt-10'>
           <img src={Sponsors} alt="Sponsors" className="w-full h-auto max-w-[800px] mx-auto rounded-lg" />
         </div>
-      </div>
       <Footer />
     </div>
   );
