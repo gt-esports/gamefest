908d6794d9fc6064b85d9db69cea09e092e99aa1
Jim Chen <loxijchen@gmail.com>
2025-04-13 15:22:31 -0400

    add highlights to selected tab in admin panel

--- src/components/adminPanel/RootAdminPanel.tsx
+++ src/components/adminPanel/RootAdminPanel.tsx
@@ -10,16 +10,36 @@ const RootAdminPanel: React.FC = () => {
   return (
     <div className="w-full rounded bg-white p-6 shadow">
       <div className="mb-4 flex space-x-4">
-        <button onClick={() => setActiveTab("staff")} className="btn">
-          Staff
+        <button
+          onClick={() => setActiveTab("staff")}
+          className={`btn ${
+            activeTab === "staff" ? "text-2xl font-bold text-tech-gold" : ""
+          }`}
+        >
+          Manage Staff
         </button>
-        <button onClick={() => setActiveTab("checkin")} className="btn">
-          Player Check-In
+        <button
+          onClick={() => setActiveTab("checkin")}
+          className={`btn ${
+            activeTab === "checkin" ? "text-2xl font-bold text-tech-gold" : ""
+          }`}
+        >
+          Player Check-in & Settings
         </button>
-        <button onClick={() => setActiveTab("points")} className="btn">
-          Points
+        <button
+          onClick={() => setActiveTab("points")}
+          className={`btn ${
+            activeTab === "points" ? "text-2xl font-bold text-tech-gold" : ""
+          }`}
+        >
+          Tokens
         </button>
-        <button onClick={() => setActiveTab("games")} className="btn">
+        <button
+          onClick={() => setActiveTab("games")}
+          className={`btn ${
+            activeTab === "games" ? "text-2xl font-bold text-tech-gold" : ""
+          }`}
+        >
           Games & Challenges
         </button>
       </div>
