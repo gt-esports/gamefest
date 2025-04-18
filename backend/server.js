4bb57f8519e4a18e2414f1567654e00927292374
Jim Chen <loxijchen@gmail.com>
2025-04-18 00:07:38 -0400

    prod

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,12 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
 // middleware
 app.use(cors());
 app.use(bodyParser.json());
